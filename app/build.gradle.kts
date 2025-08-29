febe455fe17f3b005228fd2048fd659e7fdaef2e
Sumit Gautam <sumit@tejsoft.com>
2025-08-30 00:51:02 +0530

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.navigation.safe.args)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -33,6 +36,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +52,40 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // ViewModel
+    implementation(libs.lifecycle.viewmodel.ktx)
+    //LiveData
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.common.java8)
+    implementation(libs.lifecycle.save.state)
+
+
+    // room
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
+    implementation(libs.coroutines.android)
+
+    //Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // OkHttp
+    implementation(libs.okhttp.lib)
+
+    //Dagger
+    implementation(libs.dagger.lib)
+    kapt(libs.dagger.annotationProcessor)
+
+    // Retrofit + Gson
+    implementation(libs.retrofit.lib)
+    implementation(libs.retrofit.lib.conv)
+    implementation(libs.gson.lib)
+
+    // Glide
+    implementation(libs.glide.lib)
+    kapt(libs.glide.annotationProcessor)
+
+
 }
